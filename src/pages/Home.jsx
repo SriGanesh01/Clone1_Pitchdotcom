ac5982384d0044dbb7c97becda6d4fc2c798532c
SriGanesh01 <psriganesh2005@gmail.com>
2025-03-26 19:26:46 +0530

    Initial Setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+import Footer from '../components/footer';
+
+function Home() {
+	return (
+		<div>
+			<Header />
+			<Footer />
+		</div>
+	);
+}
+export default Home;
